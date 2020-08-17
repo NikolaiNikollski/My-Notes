bda9be685e01660e931538dbee018a33535db604
unknown <nikolai-4545@mail.ru>
2020-08-18 00:25:54 +0300

    V1.0

---
+++ MyNotes/Controllers/NoteController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyNotes.Data.Models;
+using Newtonsoft.Json;
+
+namespace MyNotes.Data
+{
+    public class NoteController : Controller
+    {
+        public async void Add()
+        {
+            Request.ContentType = "Application/x-www-Form-UrlEncoded";
+
+            if (!Request.Form.ContainsKey("text"))
+            {
+                Response.ContentType = "text/html; charset=utf-8";
+                Response.StatusCode = 400;
+                await Response.WriteAsync("the parameter 'text' is missing or is set incorrectly");
+            }
+            else
+            {
+                string text = Request.Form["text"];
+                string path = @"Data/Notepad.txt";
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+                    {
+                        Note note = new Note(text);
+                        string noteJson = JsonConvert.SerializeObject(note);
+                        await sw.WriteLineAsync(noteJson);
+                    }
+                    Response.ContentType = "text/html; charset=utf-8";
+                    await Response.WriteAsync("Recording completed");
+                }
+                catch (Exception e)
+                {
+                    Response.ContentType = "text/html; charset=utf-8";
+                    Response.StatusCode = 404;
+                    await Response.WriteAsync(e.Message);
+                }
+            }
+        }
+
+        public async void List()
+        {
+            string path = @"Data/Notepad.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    List<Note> noteList = new List<Note>();
+                    while (sr.Peek() > -1)
+                    {
+                        string noteJson = await sr.ReadLineAsync();
+                        Note note = JsonConvert.DeserializeObject<Note>(noteJson);
+                        noteList.Add(note);
+                    }
+                    string noteListJson = JsonConvert.SerializeObject(noteList);
+                    Response.ContentType = "application/json; charset=utf-8";
+                    await Response.WriteAsync(noteListJson);
+                }
+            }
+            catch (Exception e)
+            {
+                Response.ContentType = "text/html; charset=utf-8";
+                Response.StatusCode = 404;
+                await Response.WriteAsync(e.Message);
+            }
+        }
+    }
+}
---
+++ MyNotes/Data/Models/Notepad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MyNotes.Data.Models
+{
+    public class Notepad
+    {
+        [JsonProperty("notes")]
+        public List<Note> NoteList;
+    }
+}
